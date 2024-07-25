879e0ed103e91070f89035b0b0b5e98b5a236260
gillty <gooee@mac.com>
2024-07-25 00:19:15 -0500

    test

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -37,20 +37,7 @@ const deleteItem = (req, res) => {
   const { itemId } = req.params;
   const userId = req.user._id;
 
-  ClothingItem.findOne({ _id: itemId })
-    .then((item) => {
-      if (!item) {
-        return res
-          .status(ERROR_CODES.NOT_FOUND)
-          .send({ message: ERROR_MESSAGES.NOT_FOUND });
-      }
-      if (item.owner.toString() !== userId.toString()) {
-        return res
-          .status(ERROR_CODES.FORBIDDEN)
-          .send({ message: ERROR_MESSAGES.FORBIDDEN });
-      }
-      return ClothingItem.findByIdAndDelete(itemId);
-    })
+  ClothingItem.findOneAndDelete({ _id: itemId, owner: userId })
     .then((deletedItem) => {
       if (!deletedItem) {
         return res
@@ -63,6 +50,11 @@ const deleteItem = (req, res) => {
     })
     .catch((err) => {
       console.error(err);
+      if (err.name === "DocumentNotFoundError") {
+        return res
+          .status(ERROR_CODES.NOT_FOUND)
+          .send({ message: ERROR_MESSAGES.NOT_FOUND });
+      }
       if (err.name === "CastError") {
         return res
           .status(ERROR_CODES.BAD_REQUEST)
